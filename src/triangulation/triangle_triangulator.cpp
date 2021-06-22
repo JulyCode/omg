4b896df9ea530ca719907da54da04b7785e86567
Julian Stahl <julian.stahl@fau.de>
2021-06-22 19:07:00 +0200

    Add wrapper for Triangle's args

--- src/triangulation/triangle_triangulator.cpp
+++ src/triangulation/triangle_triangulator.cpp
@@ -1,8 +1,6 @@
 
 #include "triangle_triangulator.h"
 
-#include <limits>
-
 #include <Triangle/jrs_triangle.h>
 #include <triangulation/triangle_helper.h>
 
@@ -30,14 +28,142 @@ void TriangleTriangulator::generateMesh(const Polygon& outline, Mesh& out_mesh) 
     // D: conforming Delaunay
     // a0.5: area constraint of 0.5
     // q25: minimum angle of 25 degrees, maximum 180 - 2 * 25
-    char args[] = "zBPupDa0.5q25";
-    jrs::triangulate(args, &in.io, &out.io, nullptr);
+    TriangleArgs args;
+    args.quiet = false;
+    args.zero_based = true;
+    args.nobound = true;
+    args.nopolywritten = true;
+    args.user_test = true;
+    args.input_type = TriangleArgs::InputType::POLY;
+    args.conformdel = true;
+    args.area_constraint = TriangleArgs::AreaConstraint::CUSTOM;
+    args.max_area = 0.5;
+    args.quality = true;
+    args.min_angle = 25;
+
+    jrs::triangulate(args.toString(), &in.io, &out.io, nullptr);
 
     out.toMesh(out_mesh);
 }
 
 int TriangleTriangulator::triunsuitable(double* v1, double* v2, double* v3, double area) {
     return 0;
+}
+
+
+TriangleArgs::TriangleArgs() : args(nullptr) {
+    input_type = InputType::POLY;
+    quality = false;
+    min_angle = 0;
+    area_constraint = AreaConstraint::NONE;
+    max_area = 0;
+    user_test = false;
+    region_attrib = false;
+    convex = false;
+    weighted = Weights::NONE;
+    jettison = false;
+    zero_based = true;
+    neighbors = false;
+    nobound = false;
+    noholes = false;
+    noexact = false;
+    conformdel = false;
+    incremental = false;
+    sweepline = false;
+    dwyer = true;
+    splitseg = false;
+    nobisect = false;
+    limit_steiner = false;
+    steiner = 0;
+    quiet = false;
+    verbose = false;
+    nopolywritten = false;
+}
+
+TriangleArgs::~TriangleArgs() {
+    if (args != nullptr) {
+        delete[] args;
+    }
+}
+
+char* TriangleArgs::toString() {
+    if (args != nullptr) {
+        throw std::runtime_error("toString can only be used once");
+    }
+
+    std::string s;
+
+    // build argument string
+    if (input_type == InputType::POLY) {
+        s += "p";
+    } else if (input_type == InputType::MESH) {
+        s += "r";
+    }
+
+    if (quality) {
+        s += "q" + std::to_string(min_angle);
+    }
+
+    if (area_constraint == AreaConstraint::FILE) {
+        s += "a";
+    } else if (area_constraint == AreaConstraint::CUSTOM) {
+        s += "a" + std::to_string(max_area);
+    }
+
+    if (user_test) { s += "u"; }
+
+    if (region_attrib) { s += "A"; }
+
+    if (convex) {  s += "c"; }
+
+    if (weighted == Weights::MODE_1) {
+        s += "w";
+    } else if (weighted == Weights::MODE_1) {
+        s += "W";
+    }
+
+    if (jettison) { s += "j"; }
+
+    if (zero_based) { s += "z"; }
+
+    if (neighbors) { s += "n"; }
+
+    if (nobound) { s += "B"; }
+
+    if (noholes) { s += "O"; }
+
+    if (noexact) { s += "X"; }
+
+    if (conformdel) { s += "D"; }
+
+    if (incremental) { s += "i"; }
+
+    if (sweepline) { s += "F"; }
+
+    if (!dwyer) { s += "l"; }
+
+    if (splitseg) { s += "s"; }
+
+    if (nobisect) { s += "Y"; }
+
+    if (limit_steiner) {
+        s += "S" + std::to_string(steiner);
+    }
+
+    if (quiet) { s += "Q"; }
+
+    if (verbose) { s += "V"; }
+
+    if (nopolywritten) { s += "P"; }
+
+    // copy arguments to new char[]
+    args = new char[s.size() + 1];
+    for (int i = 0; i < s.size(); i++) {
+        args[i] = s[i];
+    }
+    args[s.size()] = '\0';
+
+    return args;
 }
 
 }
--- src/triangulation/triangle_triangulator.h
+++ src/triangulation/triangle_triangulator.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <triangulation/triangulator.h>
+#include <geometry/types.h>
 
 namespace omg {
 
@@ -13,6 +14,103 @@ public:
 
 private:
     static int triunsuitable(double* v1, double* v2, double* v3, double area);
+};
+
+
+// not all switches are supported
+struct TriangleArgs {
+public:
+    TriangleArgs();
+    ~TriangleArgs();
+
+    enum class InputType {
+        POLY, MESH
+    };
+
+    enum class AreaConstraint {
+        NONE, FILE, CUSTOM
+    };
+
+    enum class Weights {
+        NONE, MODE_1, MODE_2
+    };
+
+    /* Triangulate a Planar Straight Line Graph (-p switch) */
+    /* or refine a previously generated mesh (-r switch). */
+    InputType input_type;
+
+    /* Quality mesh generation (-q switch). */
+    /* Minimum angle bound (specified after -q switch). */
+    bool quality;
+    real_t min_angle;
+
+    /* Apply an area constraint per triangle (-a switch without number). */
+    /* or apply a global maximum triangle area constraint (-a switch with number). */
+    AreaConstraint area_constraint;
+    real_t max_area;
+
+    /* Apply a user-defined triangle constraint (-u switch). */
+    bool user_test;
+
+    /* Apply attributes to identify triangles in certain regions. (-A switch). */
+    bool region_attrib;
+
+    /* Enclose the convex hull with segments. (-c switch). */
+    bool convex;
+
+    /* Weighted Delaunay triangulation (1 for -w switch) or regular triangulation */
+    /* ie. lower hull of a height field (2 for -W switch). */
+    Weights weighted;
+
+    /* Jettison unused vertices from output (-j switch). */
+    bool jettison;
+
+    /* Number all items starting from zero or one (0 for -z switch). */
+    bool zero_based;
+
+    /* Generate a list of triangle neighbors (-n switch). */
+    bool neighbors;
+
+    /* Suppress output of boundary information (-B switch). */
+    bool nobound;
+
+    /* Ignore holes (-O switch). */
+    bool noholes;
+
+    /* Suppress use of exact arithmetic (-X switch). */
+    bool noexact;
+
+    /* Conforming Delaunay: all triangles are truly Delaunay (-D switch). */
+    bool conformdel;
+
+    /* Use incremental method (-i switch). */
+    bool incremental;
+
+    /* Use Fortune's sweepline algorithm (-F switch). */
+    bool sweepline;
+
+    /* Use alternating cuts for divide-and-conquer (inverse of -l switch). */
+    bool dwyer;
+
+    /* Force segments into mesh by splitting instead of using CDT (-s switch). */
+    bool splitseg;
+
+    /* Suppress boundary segment splitting (-Y switch). */
+    bool nobisect;
+
+    /* Maximum number of added Steiner points (specified after -S switch).   */
+    bool limit_steiner;
+    int steiner;
+
+    bool quiet;
+    bool verbose;
+
+    bool nopolywritten;
+
+    char* toString();
+
+private:
+    char* args;
 };
 
 }
