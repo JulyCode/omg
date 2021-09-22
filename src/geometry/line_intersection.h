cfb42d9fe3bb074545e1e83ac017e523b9e886eb
Julian Stahl <julian.stahl@fau.de>
2021-09-22 16:22:58 +0200

    Improve edge case handling of line intersections

--- src/geometry/line_intersection.h
+++ src/geometry/line_intersection.h
@@ -8,6 +8,8 @@ namespace omg {
 
 using LineSegment = std::pair<const vec2_t&, const vec2_t&>;
 
+bool collinear(const LineSegment& l1, const LineSegment& l2);
+
 bool lineIntersection(const LineSegment& l1, const LineSegment& l2);
 
 std::optional<real_t> lineIntersectionFactor(const LineSegment& l1, const LineSegment& l2);
